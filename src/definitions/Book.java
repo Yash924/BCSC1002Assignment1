62008da2829278f77e702f1b3c0e06266be61bb3
Yash Garg <yash.garg_cs19@gla.ac.in>
2020-08-29 00:43:59 +0530

    created fields of Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -1,13 +1,8 @@
-/*  Created by IntelliJ IDEA.
- *  User: Yash Garg (yash924)
- *  Date: 21/08/20
- *  Time: 3:49 PM
- *  File Name : Book.java
- * */
 package definitions;
 
 public class Book {
     private String bookName;
     private String bookAuthorName;
     private String ISBN;
+
 }
