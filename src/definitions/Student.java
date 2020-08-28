9513d871060f57f91a57d8d2e704832ac9ed5f41
Yash Garg <yash.garg_cs19@gla.ac.in>
2020-08-29 01:27:11 +0530

    fields of Student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -7,5 +7,10 @@
 package definitions;
 
 public class Student {
-
+    private String studentFirstName;
+    private String getStudentMiddleName;
+    private String studentLastName;
+    private long studentUniversityRollNumber;
+    private int numberOfIssuedBooks;
+    private String[] book;
 }
