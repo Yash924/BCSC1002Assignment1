e02c823c26e770eb37a0636d906d1908872c8df4
Yash Garg <yash.garg_cs19@gla.ac.in>
2020-08-29 00:56:12 +0530

    getter and setter method

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -1,8 +1,32 @@
-package definitions;
+package definitions;/*  Created by IntelliJ IDEA.
+ *  User: Yash Garg(yash924)
+ *  Date: 21/08/20
+ *  Time: 3:50 PM
+ *  File Name : book.java
+ * */
 
 public class Book {
     private String bookName;
     private String bookAuthorName;
-    private String ISBN;
+    private String isbnNumber;
 
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName =bookName;
+    }
+
+    public String getBookAuthorName() {
+        return bookAuthorName;
+    }
+
+    public String getISBN() {
+        return isbnNumber;
+    }
+
+    public void setISBN(String isbnNumber) {
+        this.isbnNumber = isbnNumber;
+    }
 }
