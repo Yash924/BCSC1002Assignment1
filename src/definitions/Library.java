bd702f42065ae9842b11d79c38bc96d09468b225
Yash Garg <yash.garg_cs19@gla.ac.in>
2020-08-30 16:25:14 +0530

    method show books

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -44,4 +44,20 @@ public class Library {
     public int hashCode() {
         return Arrays.hashCode(getCurrentlyAvailableBooks());
 
-    }}
+    }
+    public void availableBooks(){
+        currentlyAvailableBooks[0].setBookName("java:The complete Refrence");
+        currentlyAvailableBooks[0].setBookAuthorName("Yashkant");
+        currentlyAvailableBooks[0].setIsbnNumber("8765435467546");
+        currentlyAvailableBooks[1].setBookName("Let Us Python");
+        currentlyAvailableBooks[1].setBookAuthorName("zed Shaw");
+        currentlyAvailableBooks[1].setIsbnNumber("1234567898765");
+        currentlyAvailableBooks[2].setBookName("C: Amordren Approach");
+        currentlyAvailableBooks[2].setBookAuthorName("K.N.King");
+        currentlyAvailableBooks[2].setIsbnNumber("5643289756098");
+    }
+    public void showBooks(){
+        System.out.println("Currently Available Books are: ");
+        System.out.println(Arrays.toString(currentlyAvailableBooks));
+    }
+}
